30741e01d8079751f812572bf3b16f036df67a04
lw <1026222344@qq.com>
2020-11-09 22:42:24 +0800

    项目创建完成

---
+++ gmall-admin-web/src/main/java/com/lw/gmall/admin/web/GmallAdminWebApplication.java
@@ -0,0 +1,13 @@
+package com.lw.gmall.admin.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GmallAdminWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GmallAdminWebApplication.class, args);
+    }
+
+}
